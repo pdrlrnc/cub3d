24e9199ecf9aa18f41972c6f46face1f051bb641
Pedro Lourenço <pedrolourencoemail@gmail.com>
2026-03-11 18:15:35 +0000

    norminette ok for door_teleport

--- src/door_teleport.c
+++ src/door_teleport.c
@@ -12,7 +12,7 @@
 
 #include "cub3d.h"
 
-static	void get_dx_arr(int *dx_arr)
+static void	get_dx_arr(int *dx_arr)
 {
 	dx_arr[0] = 0;
 	dx_arr[1] = 0;
@@ -20,7 +20,7 @@ static	void get_dx_arr(int *dx_arr)
 	dx_arr[3] = -1;
 }
 
-static	void get_dy_arr(int *dy_arr)
+static void	get_dy_arr(int *dy_arr)
 {
 	dy_arr[0] = 1;
 	dy_arr[1] = -1;
@@ -28,36 +28,38 @@ static	void get_dy_arr(int *dy_arr)
 	dy_arr[3] = 0;
 }
 
+static void	found_tel(t_game *game, t_norm_indexes is)
+{
+	game->player.pos_x = game->map2d[is.j + is.l * 2][is.i + is.k * 2].pos_x
+		+ (is.k < 0) * (game->grid_size - game->player.size);
+	game->player.pos_y = game->map2d[is.j + is.l * 2][is.i + is.k * 2].pos_y
+		+ (is.l < 0) * (game->grid_size - game->player.size);
+}
+
 void	try_teleport(t_game *game)
 {
-	int	 px;
-	int	 py;
-	int	 dx;
-	int	 dy;
-	int	 dx_arr[4];
-	int	 dy_arr[4];
-	int	 i;
+	t_norm_indexes	is;
+	int				dx_arr[4];
+	int				dy_arr[4];
+	int				i;
 
+	(void)is;
 	get_dx_arr(dx_arr);
 	get_dy_arr(dy_arr);
-	px = (game->player.pos_x + game->player.size / 2.0
+	is.i = (game->player.pos_x + game->player.size / 2.0
 			- game->cont2d.x1) / game->grid_size;
-	py = (game->player.pos_y + game->player.size / 2.0
+	is.j = (game->player.pos_y + game->player.size / 2.0
 			- game->cont2d.y1) / game->grid_size;
-	if (game->map2d[py][px].type != TELEPORT)
+	if (game->map2d[is.j][is.i].type != TELEPORT)
 		return ;
 	i = 0;
 	while (i < 4)
 	{
-		dx = dx_arr[i];
-		dy = dy_arr[i];
-		if (game->map2d[py + dy][px + dx].type == DOOR
-			&& game->map2d[py + dy * 2][px + dx * 2].type == TELEPORT)
-		{
-		game->player.pos_x = game->map2d[py + dy * 2][px + dx * 2].pos_x + (dx < 0) * (game->grid_size - game->player.size);
-		game->player.pos_y = game->map2d[py + dy * 2][px + dx * 2].pos_y + (dy < 0) * (game->grid_size - game->player.size);
-			return ;
-		}
+		is.k = dx_arr[i];
+		is.l = dy_arr[i];
+		if (game->map2d[is.j + is.l][is.i + is.k].type == DOOR
+			&& game->map2d[is.j + is.l * 2][is.i + is.k * 2].type == TELEPORT)
+			return (found_tel(game, is));
 		i++;
 	}
 }
